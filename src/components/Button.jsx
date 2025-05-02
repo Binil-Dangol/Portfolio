67814c1fd432b9abdf54933d958d31b4e789d640
Binil Dangol <binildangol.005@gmail.com>
2025-05-02 22:58:10 +0545

    Made adjustments

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({ text }) {
   return (
     <>
       <div className={style.buttonWrapper}>
-        <a href={`${process.env.PUBLIC_URL}/files/Binil_Dangol_CV.pdf`} download><button className={style.button}>{text}</button></a>
+        <a href={`${import.meta.env.BASE_URL}/files/Binil_Dangol_CV.pdf`} download><button className={style.button}>{text}</button></a>
       </div>
     </>
   );
